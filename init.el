5a8866c33d66fc8947269098f7795f86a292c786
Jordi Coscolla <jcoscol@thoughtworks.com>
2018-08-16 16:56:49 +0200

    ADD bookmarks keybindings
    Also trying to have flycheck :)

--- init.el
+++ init.el
@@ -8,3 +8,17 @@
 (require 'org)
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (flycheck rjsx-mode rxjs rxjs-mode evil xref-js2 js2-refactor js2-mode hydra multiple-cursors expand-region magit undo-tree which-key leuven-theme projectile diminish use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
